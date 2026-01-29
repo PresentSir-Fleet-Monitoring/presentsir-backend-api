1f06c10efa6939231dab59ed730a5977a43a5e97
Ranjit Patil <71892385+ranjit485@users.noreply.github.com>
2026-01-30 01:38:45 +0530

    Refactor BusWebClientService to use new API

--- src/main/java/com/ranjit/ps/service/BusWebClientService.java
+++ src/main/java/com/ranjit/ps/service/BusWebClientService.java
@@ -1,54 +1,30 @@
 package com.ranjit.ps.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ranjit.ps.model.Bus;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BusWebClientService {
 
     private final WebClient webClient;
-    private final ObjectMapper objectMapper;
 
-    private static final String GITHUB_URL =
-            "https://raw.githubusercontent.com/ranjit485/elite-backend-configration/main/routes.json";
-
-    public BusWebClientService(WebClient.Builder builder, ObjectMapper objectMapper) {
-        this.webClient = builder.build();
-        this.objectMapper = objectMapper;
+    public BusWebClientService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("https://api.jsonbin.io/v3/b/68fdd11ed0ea881f40bcb67c")
+                .build();
     }
 
     public List<Bus> getAllBuses() {
-        return webClient.get()
-                .uri(GITHUB_URL)
+        Mono<List<Bus>> busesMono = webClient.get()
+                .uri("?meta=false")
                 .retrieve()
-                .bodyToMono(JsonNode.class)
-                .map(this::mapToBusList)
-                .onErrorReturn(List.of())
-                .block();
-    }
+                .bodyToMono(new ParameterizedTypeReference<List<Bus>>() {});
 
-    private List<Bus> mapToBusList(JsonNode rootNode) {
-
-        if (rootNode == null || !rootNode.isArray()) {
-            return List.of();
-        }
-
-        List<Bus> buses = new ArrayList<>();
-
-        for (JsonNode node : rootNode) {
-            Bus bus = new Bus();
-            bus.setBusId(node.get("busId").asLong());
-            bus.setRouteName(node.get("routeName").asText());
-            buses.add(bus);
-        }
-
-        return buses;
+        return busesMono.block(); // blocking call
     }
 }
